fd99754570ad26021d6eb09e3cc9f4a28bc3e2d0
AniketVashishtha <53573925+AniketVashishtha@users.noreply.github.com>
2020-03-30 18:10:40 +0530

    Update rnn1.py

--- rnn1.py
+++ rnn1.py
@@ -78,7 +78,7 @@ inputs = inputs.reshape(-1,1)
 inputs = sc.transform(inputs)
 X_test = []
 
-for i in range(60, 1258):
+for i in range(60, 80):
     X_test.append(training_set_scaled[i-60:i, 0])
 X_test = np.array(X_test)
 X_test = np.reshape(X_test,(X_test.shape[0], X_test.shape[1], 1))
